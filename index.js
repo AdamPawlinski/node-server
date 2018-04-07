38b837d024a68828ab40abcf4d0c0465d9a3ea29
Adam Pawliński <adam.pawlinski@gmail.com>
2018-04-07 06:40:56 +0200

    task 13.8 finished

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+var http = require('http');
+var fs = require('fs');
 
+var server = http.createServer();
+
+fs.readFile('./index.html', 'utf-8', function(err, data){
+  server.on('request', function (request, response) {
+      response.setHeader("Content-Type", "text/html; charset=utf-8");
+      if (request.method === 'GET' && request.url === '/') {
+          response.write(data);
+              response.end();
+      } else {
+              response.statusCode = 404;
+              response.write('<img src="https://cdn-images-1.medium.com/max/1600/1*ASPrAbd2qtFVSVuru7Y7Rw.png" style="margin: 0 auto; display: flex; justify-content: center">');
+              response.end();
+      }
+  });
+})
+
+server.listen(9000);
